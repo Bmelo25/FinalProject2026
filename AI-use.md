d383ed6094589ee44d9ad689980d910197d16156
Bmelo25 <belmel29@bergen.org>
2026-05-24 22:32:33 -0400

    Succsefully linked API and connected it to input
    Co-authored-by: tylzho29-a11y <tylzho29-a11y@users.noreply.github.com>

--- AI-use.md
+++ AI-use.md
@@ -2,3 +2,4 @@ https://claude.ai/share/e0014f7a-2c12-4b19-bfae-18c5d3d9d77a
 
 Asked AI with help for creating a grid
 Asked a question on how the input value works
+Asked my AI how to correctly set up the API and asked it about what my errors meant (I also used the help sheet given on google clasroom for API stucture )
--- Project/script.js
+++ Project/script.js
@@ -4,18 +4,19 @@ answer = document.querySelectorAll(".answer-box-1")
 guess.addEventListener('keydown', (e) =>{
     
     if (e.key === "Enter"){
+        celebrityData()
         answer.value = guess.value
         
         answer.forEach(ans => {
        ans.value = answer.value
     });
-    answerChecker()
-    if (answer.value === correctAnswer ){
-        // so imagine this if statement but multiple tries and answers in the API
-      console.log(correctAnswer)
-      answer.value = 'correct!!!'
-      answer.classList.add("")
-    }
+    // answerChecker()
+    // if (answer.value === correctAnswer ){
+    //     // so imagine this if statement but multiple tries and answers in the API
+    //   console.log(correctAnswer)
+    //   answer.value = 'correct!!!'
+    //   answer.classList.add("")
+    // }
 }
 })
 const correctAnswer = "hello"
@@ -30,13 +31,29 @@ function settings(){
 }
 
 
-function celebritySearch(){
 // This is going to be fetching the API and generating a random celebrity, assigning them to correct variables and 
+const celebrityData = async () => {
+const celebrity = guess.value
+ if(!celebrity) return
+try{
+    const url = `https://api.api-ninjas.com/v1/celebrity?name=${celebrity}`
+
+        const response = await fetch(url, {
+        headers: { 'X-Api-Key': 'qR8GaBspsOHmoycr4KLkodVwTi3uMJjEDtd0UXr5'}
+        
+    })
+    const data = await response.json()
+    console.log(data)
+    // displayData(data);
+        }
+        catch (error) {
+console.log(error)
+}
 }
 
+celebrityData()
 
 function answerChecker(){
 // This will check if the answer is correct adding to a counter
-
 
 }
