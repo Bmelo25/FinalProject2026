923bde3993d75ee0e43396af118799446e34685a
Bmelo25 <belmel29@bergen.org>
2026-05-26 17:30:42 -0400

    Added a second API to create both a guessing system and a random generation feature (Can only generate one)

--- Project/script.js
+++ Project/script.js
@@ -1,22 +1,19 @@
 guess = document.querySelector(".guess")
 answer = document.querySelectorAll(".answer-box-1")
-
+card = document.querySelector(".card")
 guess.addEventListener('keydown', (e) =>{
     
     if (e.key === "Enter"){
-        celebrityData()
+        celebritySearch()
         answer.value = guess.value
         
         answer.forEach(ans => {
        ans.value = answer.value
     });
+    // note for Tyler: put the answer correctness function in here
+    // 
     // answerChecker()
-    // if (answer.value === correctAnswer ){
-    //     // so imagine this if statement but multiple tries and answers in the API
-    //   console.log(correctAnswer)
-    //   answer.value = 'correct!!!'
-    //   answer.classList.add("")
-    // }
+
 }
 })
 const correctAnswer = "hello"
@@ -32,9 +29,9 @@ function settings(){
 
 
 // This is going to be fetching the API and generating a random celebrity, assigning them to correct variables and 
-const celebrityData = async () => {
-const celebrity = guess.value
- if(!celebrity) return
+const celebritySearch = async () => {
+const celebrity = guess.value.toLowerCase()
+    if(!celebrity) return
 try{
     const url = `https://api.api-ninjas.com/v1/celebrity?name=${celebrity}`
 
@@ -51,9 +48,34 @@ console.log(error)
 }
 }
 
-celebrityData()
 
+celebritySearch()
+
+
+// this is the randomly generated celebrity
+const generateCelebrity = async () => {
+    try{
+       const url = `https://celebrities-api-by-apirobots.p.rapidapi.com/v1/celebrities/random`
+
+        const response = await fetch(url, {
+        headers: {'x-rapidapi-key': '4e7be9fa07mshf4492be2be6ca62p1132bbjsnba7f74105220',
+		'x-rapidapi-host': 'celebrities-api-by-apirobots.p.rapidapi.com',
+		'Content-Type': 'application/json'
+        }
+    })
+    
+    const answerdata = await response.json()
+    const correctName = answerdata.name
+    const correctNationality = answerdata.nationality
+    const correctGender = answerdata.gender
+    console.log(correctGender)
+    console.log(answerdata)
+}
+catch (error) {
+    console.log(error)
+}
+}
+generateCelebrity()
 function answerChecker(){
 // This will check if the answer is correct adding to a counter
-
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FinalProject2026
+
+Just a couple of quick things: The API used generates only one random celebrity due to it being free tier
